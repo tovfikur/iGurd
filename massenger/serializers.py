cb648b9fc33e8d1c141709062059fa385a19ccc7
tovfikur <tovfikurrahman@gmail.com>
2020-08-03 22:26:37 +0600

    iGurd

--- massenger/serializers.py
+++ massenger/serializers.py
@@ -3,16 +3,12 @@ from .models import Chat
 
 
 class MassageSerializer(serializers.ModelSerializer):
-    Replays = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Chat
         fields = [
             'id',
             'UserID',
-            'Subject',
-            'Replays',
             'Text',
-            'Importance',
             'Time',
             'Seen'
         ]
