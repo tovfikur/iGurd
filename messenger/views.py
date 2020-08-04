3dabb8749b5698661d669fa360a7f29ccd2cf97d
tovfikur <tovfikurrahman@gmail.com>
2020-08-04 12:05:06 +0600

    iGurd

--- messenger/views.py
+++ messenger/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics,views
 from rest_framework.response import Response
 from .models import Chat,ChatBody
+from user.models import UserToken
 from .serializers import ChatSerializer, ChatBodySerializer
 # Create your views here.
 
 
 class Massages(generics.ListCreateAPIView):
     serializer_class = ChatBodySerializer
-    queryset = ChatBody.objects.all()
+
+    def get_queryset(self):
+        print(self.request.META)
+
+        return ChatBody.objects.all()
