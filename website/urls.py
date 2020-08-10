19fa54a95472669affb7c3e824ef469ee22272c8
tovfikur <tovfikurrahman@gmail.com>
2020-08-10 12:59:52 +0600

    iGurd

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('', views.index),
+    path('login', views.login),
+    path('toc', views.toc),
+    path('otp', views.phone_otp),
+    path('signup', views.signup),
+    path('restore', views.restore_password),
+    path('privacy', views.privacy),
+    path('join', views.join_as_partner),
+    path('inside', views.inside_escrow),
+    path('calc', views.cash_calculator),
+    path('edit',  views.edit_ac),
+    path('dash', views.dashboard),
+    path('contact', views.contact)
 ]
--- website/views.py
+++ website/views.py
@@ -2,41 +2,54 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request,'iGurd/index.html',{})
 
-def contact(request):
+def index(request):
     return render(request, 'iGurd/index.html', {})
 
+
+def contact(request):
+    return render(request, 'iGurd/innerPages/contact.html', {})
+
+
 def dashboard(request):
-    pass
+    return render(request, 'iGurd/innerPages/Dashboard.html', {})
 
-def editac(request):
-    pass
 
-def cashcalculator(request):
-    pass
+def edit_ac(request):
+    return render(request, 'iGurd/innerPages/edit-account.html', {})
+
+
+def cash_calculator(request):
+    return render(request, 'iGurd/innerPages/fee-calculator.html', {})
+
 
 def inside_escrow(request):
-    pass
+    return render(request, 'iGurd/innerPages/insideEscrow.html', {})
+
 
 def join_as_partner(request):
-    pass
+    return render(request, 'iGurd/innerPages/join-as-a-partner.html', {})
+
 
 def login(request):
-    pass
+    return render(request, 'iGurd/innerPages/login.html', {})
+
 
 def privacy(request):
-    pass
+    return render(request, 'iGurd/innerPages/privacy.html', {})
+
 
 def restore_password(request):
-    pass
+    return render(request, 'iGurd/innerPages/restorePassword.html', {})
+
 
 def signup(request):
-    pass
+    return render(request, 'iGurd/innerPages/signup.html', {})
+
 
 def phone_otp(request):
-    pass
+    return render(request, 'iGurd/innerPages/signup2.html', {})
+
 
 def toc(request):
-    pass
+    return render(request, 'iGurd/innerPages/toc.html', {})
